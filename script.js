860fe2b4a8e07297d55141e93be14d0c5e25e619
Hanbi58 <hanxiaonan2012@gmail.com>
2022-09-22 18:19:55 -0700

    Teacher's version done

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeEL = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
+
 let size = 20;
-let color = "black";
+let isPressed = false;
+let color = colorEl.value;
+let x, y;
+
+colorEl.addEventListener("change", (e) => {
+  color = e.target.value;
+});
+
+increaseBtn.addEventListener("click", () => {
+  size = Math.min(40, size + 5);
+  sizeEL.innerText = size;
+});
+
+decreaseBtn.addEventListener("click", () => {
+  size = Math.max(5, size - 5);
+  sizeEL.innerText = size;
+});
+
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+
+  x = e.offsetX;
+  y = e.offsetY;
+  drawCircle(x, y);
+});
+canvas.addEventListener("mouseup", (e) => {
+  isPressed = false;
+  x = undefined;
+  y = undefined;
+});
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+    x = x2;
+    y = y2;
+  }
+});
+
 function drawCircle(x, y) {
   ctx.beginPath();
   ctx.arc(x, y, size, 0, Math.PI * 2);
@@ -13,10 +60,14 @@ function drawLine(x1, y1, x2, y2) {
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
-  ctx.stokeStyle = color;
-  ctx.lineWidth = size;
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
+
+clearEl.addEventListener("click", () =>
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+);
 // function clickToDraw(e) {
 //   const rect = canvas.getBoundingClientRect();
 //   const x = e.clientX - rect.left;
